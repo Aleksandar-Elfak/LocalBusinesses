0930858b36404a1916aaf8e43dee9ea248383fc6
Aleksandar-Elfak <77788823+Aleksandar-Elfak@users.noreply.github.com>
2022-01-05 17:59:42 +0100

    garg

--- Server/Controllers/BusinessController.cs
+++ Server/Controllers/BusinessController.cs
@@ -17,10 +17,39 @@ namespace Server.Controllers
         }
 
         [HttpGet]
-        [Route("GetBusiness/{name}")]
-        public async Task<Business> GetBusiness(string name)
+        [Route("GetBusiness/{name}&{username}")]
+        public async Task<Business> GetBusiness(string name, string username)
         {
-            return await data.GetBusiness(name);
+            return await data.GetBusiness(name, username);
+        }
+
+        [HttpGet]
+        [Route("GetRecommended/{username}")]
+        public async Task<List<Business>> GetRecommended(string username)
+        {
+            return await data.GetRecommended(username);
+        }
+
+        [HttpGet]
+        [Route("GetReviews/{username}")]
+        public async Task<List<Review>> GetReviews(string username)
+        {
+            return await data.GetReviews(username);
+        }
+
+        [HttpGet]
+        [Route("SearchBusinesses/{tag}")]
+        public async Task<List<Business>> SearchBusinesses(string tag)
+        {
+            return await data.SearchBusinesses(tag);
+        }
+
+        [HttpPost]
+        [Route("Review/{username}&{name}")]
+        public IActionResult Review([FromBody] Review r, string username, string name)
+        {
+            data.Review(username, name, r);
+            return Ok();
         }
 
         [HttpPost]
--- Server/Services/DataProvider.cs
+++ Server/Services/DataProvider.cs
@@ -12,18 +12,65 @@ namespace Server.Services
             Session = new Neo4JDriver().Session;
         }
 
-        public async Task<Business> GetBusiness(string name)
+        public async Task<Business> GetBusiness(string name, string username)
         {
-            var a = (await (await Session.RunAsync($"Match (b:Business) where b.name = '{name}' return b")).ToListAsync()).FirstOrDefault();
+            var a = (await (await Session.RunAsync($"Match (b:Business) where b.name = '{name}' merge(u: User {{ username: '{username}'}})" +
+                $"merge(u)-[r: Visited]->(b) on create set r.visits = 1 on match set r.visits = r.visits + 1 return b")).ToListAsync()).FirstOrDefault();
 
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
             Business b = a["b"].As<INode>().ToObject<Business>();
-            System.Console.WriteLine($"{b.Name} {b.Address} {b.Rating.ToString()}");
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
             return b;
         }
 
         public void LogIn(User user)
         {
             Session.RunAsync($"Merge (u:User {{username: '{user.Username}', password: '{user.Password}'}})");
+        }
+
+        public async void Review(string username, string business, Review r)
+        {
+            await Session.RunAsync($"Match (b:Business) where b.name = '{business}' merge(u: User {{ username: '{username}'}})" + 
+                $"merge(u)-[r: Reviewed]->(b) set r.review = '{r.review}' set r.rating = { r.Rating}");
+            ReRate(business);
+        }
+
+        public void ReRate(string name)
+        {
+            Session.RunAsync($"MATCH ()-[r:Reviewed]->(b:Business) where b.name = '{name}' with b, avg(r.rating) as d set b.rating = d");
+        }
+
+        public async Task<List<Review>> GetReviews(string name)
+        {
+            List<Review> reviews = new List<Review>();
+            var list = await (await Session.RunAsync($"Match (u:User)-[r:Reviewed]->() where u.username = '{name}' return r limit 5")).ToListAsync();
+            foreach (var item in list)
+            {
+                reviews.Add(item["r"].As<IRelationship>().ToObject<Review>());
+            }
+            return reviews;
+        }
+
+        public async Task<List<Business>> SearchBusinesses(string tag)
+        {
+            List<Business> businesses = new List<Business>();
+            var list = await(await Session.RunAsync($"Match (b:Business)-[:tagged]->(t:tag) where t.tag = '{tag}' return b limit 5")).ToListAsync();
+            foreach (var item in list)
+            {
+                businesses.Add(item["b"].As<INode>().ToObject<Business>());
+            }
+            return businesses;
+        }
+
+        public async Task<List<Business>> GetRecommended(string username)
+        {
+            List<Business> businesses = new List<Business>();
+            var list = await (await Session.RunAsync($"Match (u:User)-[v:Visited]->(b:Business) where u.username = '{username}' return b order by b.rating desc limit 5")).ToListAsync();
+            foreach (var item in list)
+            {
+                businesses.Add(item["b"].As<INode>().ToObject<Business>());
+            }
+            return businesses;
         }
     }
 }
