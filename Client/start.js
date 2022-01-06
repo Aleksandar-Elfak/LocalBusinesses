339ae704faa1c9fce49cacb49d83981112fa6683
Aleksandar-Elfak <77788823+Aleksandar-Elfak@users.noreply.github.com>
2022-01-06 14:24:27 +0100

    login

--- Client/start.js
+++ Client/start.js
@@ -1,61 +1,84 @@
 import { UserView } from "./userView.js";
 
-
 export class Start {
-    constructor() {
-        this.container = null;
-        this.username = null;
-    }
+	constructor() {
+		this.container = null;
+		this.username = null;
+	}
 
-    draw(host) {
-        if (!host) throw new Error("Host is not defined");
+	draw(host) {
+		if (!host) throw new Error("Host is not defined");
 
-        this.container = document.createElement("div");
-        this.container.className = "mainDiv1";
-        host.appendChild(this.container);
+		this.container = document.createElement("div");
+		this.container.className = "mainDiv1";
+		host.appendChild(this.container);
 
-        const left = document.createElement("div");
-        left.className = "leftDiv1";
-        this.container.appendChild(left);
+		const left = document.createElement("div");
+		left.className = "leftDiv1";
+		this.container.appendChild(left);
 
-        const title = document.createElement("div");
-        title.className = "title1";
-        left.appendChild(title);
+		const title = document.createElement("div");
+		title.className = "title1";
+		left.appendChild(title);
 
-        const mainTitle = document.createElement("div");
-        mainTitle.className = "mainTitle1";
-        mainTitle.innerHTML = "Local Businesses";
-        title.appendChild(mainTitle);
+		const mainTitle = document.createElement("div");
+		mainTitle.className = "mainTitle1";
+		mainTitle.innerHTML = "Local Businesses";
+		title.appendChild(mainTitle);
 
-        const subTitle = document.createElement("div");
-        subTitle.className = "subTitle1";
-        subTitle.innerHTML = "Find the service you need";
-        title.appendChild(subTitle);
+		const subTitle = document.createElement("div");
+		subTitle.className = "subTitle1";
+		subTitle.innerHTML = "Find the service you need";
+		title.appendChild(subTitle);
 
-        const loginDiv = document.createElement("div");
-        loginDiv.className = "loginDiv1";
-        loginDiv.innerHTML = "aaaaaaaaaaaaaaaaaaaaaa";
-        left.appendChild(loginDiv);
+		const loginDiv = document.createElement("div");
+		loginDiv.className = "loginDiv1";
+		left.appendChild(loginDiv);
 
-        const right = document.createElement("div");
-        right.className = "rightDiv1";
-        this.container.appendChild(right);
+		const loginLine = document.createElement("div");
+		loginLine.className = "loginLine1";
+		loginDiv.appendChild(loginLine);
 
-        const rightImg = document.createElement("img");
-        rightImg.className = "img1";
-        rightImg.src = "img1.png"
-        right.appendChild(rightImg);
+		const lbl1 = document.createElement("label");
+		lbl1.classList = "ui label yellow";
+		lbl1.innerHTML = "Username: ";
+		loginLine.appendChild(lbl1);
 
+		const input1 = document.createElement("input");
+		input1.className = "ui large";
+		loginLine.appendChild(input1);
 
-        //deo gde se crta user ili biznis
+		const loginLine1 = document.createElement("div");
+		loginLine1.className = "loginLine1";
+		loginDiv.appendChild(loginLine1);
 
-        const testButton = document.createElement("button")
-        testButton.innerHTML = "User";
-        testButton.onclick = () => {
-            document.body.removeChild(this.container);
-            let u = new UserView(this);
-            u.draw();
-        }
-        right.appendChild(testButton);
-    }
+		const lbl2 = document.createElement("label");
+		lbl2.classList = "ui label yellow";
+		lbl2.innerHTML = "Password: ";
+		loginLine1.appendChild(lbl2);
+
+		const input2 = document.createElement("input");
+		input2.className = "ui large";
+		loginLine1.appendChild(input2);
+
+		const testButton = document.createElement("button");
+		testButton.className = "ui button yellow";
+		testButton.innerHTML = "Log in";
+		testButton.onclick = () => {
+			document.body.removeChild(this.container);
+			let u = new UserView(this, "Vule");
+			u.draw();
+		};
+		loginDiv.appendChild(testButton);
+
+		const right = document.createElement("div");
+		right.className = "rightDiv1";
+		right.style.height = window.innerHeight;
+		this.container.appendChild(right);
+
+		const rightImg = document.createElement("img");
+		rightImg.className = "img1";
+		rightImg.src = "img1.png";
+		right.appendChild(rightImg);
+	}
 }
--- Client/userView.js
+++ Client/userView.js
@@ -1,32 +1,32 @@
 import { BusinessView } from "./businessView.js";
 
 export class UserView {
-    constructor(start) {
-        this.container = null;
-        this.username = null;
-        this.start = start;
-    }
+	constructor(start, username) {
+		this.container = null;
+		this.username = username;
+		this.start = start;
+	}
 
-    draw() {
-        this.container = document.createElement("div");
-        this.container.innerHTML = "User view";
-        document.body.appendChild(this.container);
+	draw() {
+		this.container = document.createElement("div");
+		this.container.innerHTML = "User view";
+		document.body.appendChild(this.container);
 
-        const testButton2 = document.createElement("button")
-        testButton2.innerHTML = "back";
-        testButton2.onclick = () => {
-            document.body.removeChild(this.container);
-            this.start.draw(document.body)
-        }
-        this.container.appendChild(testButton2);
+		const testButton2 = document.createElement("button");
+		testButton2.innerHTML = "back";
+		testButton2.onclick = () => {
+			document.body.removeChild(this.container);
+			this.start.draw(document.body);
+		};
+		this.container.appendChild(testButton2);
 
-        const testButton = document.createElement("button")
-        testButton.innerHTML = "User";
-        testButton.onclick = () => {
-            document.body.removeChild(this.container);
-            let u = new BusinessView(this);
-            u.draw();
-        }
-        this.container.appendChild(testButton);
-    }
+		const testButton = document.createElement("button");
+		testButton.innerHTML = "User";
+		testButton.onclick = () => {
+			document.body.removeChild(this.container);
+			let u = new BusinessView(this);
+			u.draw();
+		};
+		this.container.appendChild(testButton);
+	}
 }
